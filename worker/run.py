1508db8ed2b276773818849e5413a341ed13eb8a
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-05-21 11:47:17 +0200

    I-3: Implementing the first activity.

--- worker/run.py
+++ worker/run.py
@@ -16,9 +16,15 @@ def sample_external_task(task: ExternalTask) -> TaskResult:
     print("sample_external_task")
     return task.complete({"varFromPython": "Greetings from Python"})
 
+
+def determine_type_of_customer(task: ExternalTask) -> TaskResult:
+    return task.complete({"CUSTOMER_TYPE": "CORPORATE"})
+
 def main():
     print("Starting the worker")
-    ExternalTaskWorker(worker_id="1", config=default_config, base_url="http://localhost:8081/engine-rest").subscribe("sample_external_task", sample_external_task)
+    ExternalTaskWorker(worker_id="1", config=default_config, base_url="http://localhost:8081/engine-rest")
+    .subscribe("sample_external_task", sample_external_task)
+    .subscribe("determine_type_of_customer", determine_type_of_customer)
 
 if __name__ == '__main__':
     main()   
