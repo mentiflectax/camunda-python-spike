2e50d6ffe59bad991c79a10daeb25ea831440fd9
Mentiflectax <1143055+mentiflectax@users.noreply.github.com>
2022-05-21 12:46:57 +0200

    I-3: Gateway example (#6)
    Co-authored-by: Dmitrii Pisarenko <dp@dpisarenko.com>

--- worker/run.py
+++ worker/run.py
@@ -9,16 +9,38 @@ default_config = {
     "asyncResponseTimeout": 5000,
     "retries": 3,
     "retryTimeout": 5000,
-    "sleepSeconds": 30
+    "sleepSeconds": 30,
+    "isDebug": True
 }
 
 def sample_external_task(task: ExternalTask) -> TaskResult:
     print("sample_external_task")
     return task.complete({"varFromPython": "Greetings from Python"})
 
-def main():
-    print("Starting the worker")
-    ExternalTaskWorker(worker_id="1", config=default_config, base_url="http://localhost:8081/engine-rest").subscribe("sample_external_task", sample_external_task)
 
+def determine_type_of_customer(task: ExternalTask) -> TaskResult:
+    print("determine_type_of_customer")
+    customer_nr = task.get_variable("CUSTOMER_NUMBER")
+    if ((customer_nr % 2) == 0):
+        customer_type = "CORPORATE"
+    else:
+        customer_type = "INDIVIDUAL"
+    return task.complete({"CUSTOMER_TYPE": customer_type})
+
+def main():
+    configure_logging()
+    topics = [
+        ("sample_external_task", sample_external_task),
+        ("determine_type_of_customer", determine_type_of_customer)
+    ]
+    executor = ThreadPoolExecutor(max_workers=len(topics))
+    for index, topic_handler in enumerate(topics):
+        topic = topic_handler[0]
+        handler_func = topic_handler[1]
+        executor.submit(ExternalTaskWorker(worker_id=index, config=default_config, base_url="http://localhost:8081/engine-rest").subscribe, topic, handler_func)
+
+def configure_logging():
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=[logging.StreamHandler()])
+    
 if __name__ == '__main__':
     main()   
