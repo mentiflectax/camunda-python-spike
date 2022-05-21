b2437ef1aadb01bcf31ac614c6d0b6ee98817031
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-05-21 13:17:04 +0200

    I-4: Bugfix

--- worker/run.py
+++ worker/run.py
@@ -29,13 +29,14 @@ def determine_type_of_customer(task: ExternalTask) -> TaskResult:
 
 
 def provoke_technical_failure(task: ExternalTask) -> TaskResult:
-    return task.failure(error_message="Technical error occurred",  error_details="Demonstration of simulating a technical error", max_retries=3, retry_timeout=5000)
+    return task.failure(error_message="Technical error occurred",  error_details="Demonstration of simulating a technical error", max_retries=0, retry_timeout=5000)
 
 def main():
     configure_logging()
     topics = [
         ("sample_external_task", sample_external_task),
-        ("determine_type_of_customer", determine_type_of_customer)
+        ("determine_type_of_customer", determine_type_of_customer),
+        ("provoke_technical_failure", provoke_technical_failure)
     ]
     executor = ThreadPoolExecutor(max_workers=len(topics))
     for index, topic_handler in enumerate(topics):
