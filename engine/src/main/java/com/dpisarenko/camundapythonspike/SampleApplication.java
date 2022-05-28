e69e8d43fa501d177a34b1bb4e54957c7bef4c3c
Mentiflectax <1143055+mentiflectax@users.noreply.github.com>
2022-05-28 18:41:14 +0200

    I-9: Pythonic script tasks and conditions (#10)

--- engine/src/main/java/com/dpisarenko/camundapythonspike/SampleApplication.java
+++ engine/src/main/java/com/dpisarenko/camundapythonspike/SampleApplication.java
@@ -1,6 +1,7 @@
 package com.dpisarenko.camundapythonspike;
 
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.python.core.Options;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableProcessApplication
 public class SampleApplication {
     public static void main(String... args){
+        Options.importSite = false;
         SpringApplication.run(SampleApplication.class, args);
     }
 }
