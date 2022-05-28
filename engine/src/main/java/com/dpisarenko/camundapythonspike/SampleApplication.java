b9d9c9446e359977477d249668af3945c31b5a51
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-05-28 15:38:40 +0200

    I-9: Now a minimal Pythonic script task works.

--- engine/src/main/java/com/dpisarenko/camundapythonspike/SampleApplication.java
+++ engine/src/main/java/com/dpisarenko/camundapythonspike/SampleApplication.java
@@ -1,13 +1,34 @@
 package com.dpisarenko.camundapythonspike;
 
+import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.python.core.Options;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptEngineManager;
 
 @SpringBootApplication
 @EnableProcessApplication
 public class SampleApplication {
+    @Autowired
+    private ProcessEngine engine;
+
     public static void main(String... args){
+        // ScriptEngineManager.
+        Options.importSite = false;
+        final ScriptEngineManager manager = new ScriptEngineManager();
+        final ScriptEngine engine = manager.getEngineByName("jython");
+
         SpringApplication.run(SampleApplication.class, args);
+    }
+
+    @PostConstruct
+    public void test() {
+        System.out.println("Hello");
     }
 }
