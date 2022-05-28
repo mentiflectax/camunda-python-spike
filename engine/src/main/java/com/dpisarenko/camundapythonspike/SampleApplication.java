f25ba32fda1743c6815b256c5e5ed2d04a6ca4ba
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-05-28 15:49:52 +0200

    I-9: Added basic documentation.

--- engine/src/main/java/com/dpisarenko/camundapythonspike/SampleApplication.java
+++ engine/src/main/java/com/dpisarenko/camundapythonspike/SampleApplication.java
@@ -1,16 +1,9 @@
 package com.dpisarenko.camundapythonspike;
 
-import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.python.core.Options;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineFactory;
-import javax.script.ScriptEngineManager;
 
 @SpringBootApplication
 @EnableProcessApplication
